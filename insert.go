2b2ae623ba4da056ba248196cbe2fac093b4d1f9
Ivar Johansson <ivar@webbmaffian.se>
2022-06-06 18:03:46 +0200

    Added `BulkInsert()`

--- insert.go
+++ insert.go
@@ -39,3 +39,46 @@ func Insert(table string, src any) (err error) {
 
 	return
 }
+
+func BulkInsert(table string, columns []string, insert func() []any) (err error) {
+	ctx := context.Background()
+	poolConn, err := db.Acquire(ctx)
+
+	if err != nil {
+		return
+	}
+
+	defer poolConn.Release()
+
+	conn := poolConn.Conn()
+	numColumns := len(columns)
+	placeholders := make([]string, 0, numColumns)
+
+	for i := 0; i < numColumns; i++ {
+		placeholders = append(placeholders, "$"+strconv.Itoa(i+1))
+	}
+
+	sd, err := conn.Prepare(ctx, "stmt_insert_"+table, "INSERT INTO "+table+" ("+strings.Join(columns, ", ")+") VALUES ("+strings.Join(placeholders, ", ")+")")
+
+	if err != nil {
+		return
+	}
+
+	defer conn.Deallocate(ctx, sd.Name)
+
+	for {
+		values := insert()
+
+		if values == nil {
+			break
+		}
+
+		_, err = conn.Exec(ctx, sd.Name, values...)
+
+		if err != nil {
+			break
+		}
+	}
+
+	return
+}
