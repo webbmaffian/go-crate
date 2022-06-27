e61ebb97661aa518ac9b0717aa630c0c548c98c7
Ivar Johansson <ivar@webbmaffian.se>
2022-06-27 11:14:44 +0200

    Fixed join bug

--- select_join.go
+++ select_join.go
@@ -13,25 +13,19 @@ func (j *InnerJoin) run(args *[]any) string {
 	return "INNER JOIN " + j.Table + " ON " + j.Condition.run(args)
 }
 
-type OuterJoin struct {
-	InnerJoin
-}
+type OuterJoin InnerJoin
 
 func (j *OuterJoin) run(args *[]any) string {
 	return "OUTER JOIN " + j.Table + " ON " + j.Condition.run(args)
 }
 
-type LeftJoin struct {
-	InnerJoin
-}
+type LeftJoin InnerJoin
 
 func (j *LeftJoin) run(args *[]any) string {
 	return "LEFT JOIN " + j.Table + " ON " + j.Condition.run(args)
 }
 
-type RightJoin struct {
-	InnerJoin
-}
+type RightJoin InnerJoin
 
 func (j *RightJoin) run(args *[]any) string {
 	return "RIGHT JOIN " + j.Table + " ON " + j.Condition.run(args)
